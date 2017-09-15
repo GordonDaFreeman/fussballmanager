95ef8437c163511a032c1ed810f4758fa9ec5c88
Delta1805 <delta1805@users.noreply.github.com>
2017-09-15 16:00:13 +0200

    Fusion :boom:

--- src/fußballmanager/Spiel.java
+++ src/fußballmanager/Spiel.java
@@ -1,19 +1,44 @@
-package fuﬂballmanager;
+package fussballmanager;
 
-import java.util.Random;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 
-import fuﬂballmanager.personen.Schiedsrichter;
+import java.text.NumberFormat;
+
+import fussballmanager.personen.*;
 
 public class Spiel {
-	//Spielzeit wird in Sekunden
 	int spielzeit;
 	int verl‰ngerung;
 	Team heimteam;
 	Team ausw‰rtsteam;
 	Schiedsrichter schiri;
-	
-	public void tick(){
-		spielzeit++;
-		int zufall = new Random().nextInt(100);
+	boolean werAnstoss;
+
+	public Spiel(Team heim, Team aus, Schiedsrichter s) {
+		this.heimteam = heim;
+		this.ausw‰rtsteam = aus;
+		this.schiri = s;
+		werAnstoss = ((int) Math.random()) == 0 ? true : false;
+		spielzeit = 0;
 	}
+
+	public void tick() {
+		spielzeit++;
+
+	}
+
+	public String getSpielzeit() {
+		String r = "";
+		NumberFormat numberFormat = new DecimalFormat("0");
+		numberFormat.setRoundingMode(RoundingMode.DOWN);
+		r += "Spielzeit: " + numberFormat.format(this.spielzeit / 60) + ":";
+		if (spielzeit % 60 < 10){
+			r += "0";
+		}
+		r += spielzeit % 60;
+		return r;
+	}
+	
+	
 }
--- src/fußballmanager/personen/Trainer.java
+++ src/fußballmanager/personen/Trainer.java
@@ -1,5 +1,29 @@
-package fuﬂballmanager.personen;
+package fussballmanager.personen;
 
-public class Trainer {
-
+public class Trainer extends Person{
+	int inspiration;
+	int erfahrung = 0;
+	
+	public Trainer(String name, String vorname, int inspiration, int erfahrung){
+		this.name = name;
+		this.vorname = vorname;
+		this.inspiration = inspiration;
+		this.erfahrung = erfahrung;
+	}
+	
+	public Trainer(){
+		this.name ="Mustertrainer";
+		this.vorname = "Max";
+		this.inspiration = (int) (Math.random()*100);
+	}
+	
+	public Trainer(String vorname, String name, int inspiration){
+		this.name = name;
+		this.vorname = vorname;
+		this.inspiration = inspiration;
+	}
+	
+	
+	
+	
 }
