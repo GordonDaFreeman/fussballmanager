d4585195e3bd2d4416b2d1c0c725776675891c99
GordonDaFreeman <gologo.x@gmail.com>
2017-09-20 18:15:08 +0200

    added isInRange(Position p)

--- src/fußballmanager/Flugbahn.java
+++ src/fußballmanager/Flugbahn.java
@@ -6,15 +6,24 @@ public class Flugbahn {
 	//Definitionsbereich x1 und x2 + y1 und y2
 	
 	double m,b;
-	//double x1,x2,y1,y2;
+	Position p1,p2;
 	
 	public Flugbahn(Position p1,Position p2) {
 		m = (p1.y-p2.y)/(p1.x-p2.x);
 		b = p1.y+m*p1.x;
 	}
 	
-	//Moritz mach das mal :D
-	public double getDistancToPosition(Position p){
-		return 0;
+	public double f(double x){
+		return m*x+b;
+	}
+	
+	public boolean isInRange(Position p){
+		if(p1.x<p.x&&p2.x>p.x||p1.x>p.x&&p2.x<p.x){
+			//Punkt liegt im Definitionsbereich
+			if(Math.abs(p.y-f(p.x))<0.5){
+				return true;
+			}
+		}
+		return false;
 	}
 }
