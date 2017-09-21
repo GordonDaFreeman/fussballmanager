40d309bf3fe5e246d77eb61cf4b4eba93562b314
GordonDaFreeman <gologo.x@gmail.com>
2017-09-21 12:54:32 +0200

    Update Torwart.java

--- src/fußballmanager/personen/Torwart.java
+++ src/fußballmanager/personen/Torwart.java
@@ -1,18 +1,19 @@
-package fuﬂballmanager.personen;
+package fu√üballmanager.personen;
 
 import java.util.Random;
 
-import fuﬂballmanager.namegen.Datenbank;
+import fu√üballmanager.namegen.Datenbank;
 
 public class Torwart extends Spieler{
 	
 	public Torwart() {
 		int random = new Random().nextInt(100);
 		this.verteidigung = random;
-		this.st‰rke = 100-random;
+		this.st√§rke = 100-random;
 		this.geschwindigkeit = 4+Math.random()*2;
 		this.vorname = Datenbank.genVorname();
 		this.name = Datenbank.genName();
+		p = new Position(0,0);
 	}
 
 }
