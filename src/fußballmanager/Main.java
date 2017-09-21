e81073918b1687b27a7bab8975529db45f43708a
delta1805 <31958216+delta1805@users.noreply.github.com>
2017-09-21 12:54:57 +0200

    Update Main.java

--- src/fußballmanager/Main.java
+++ src/fußballmanager/Main.java
@@ -1,8 +1,27 @@
-package fu�ballmanager;
+package fußballmanager;
+
+import fußballmanager.personen.Schiedsrichter;
 
 public class Main {
+	static Team t1;
+	static Team t2;
+	static Schiedsrichter schiri;
+	static Spiel spiel;
 	
 	public static void main(String[] args) {
+		t1 = new Team();
+		t2 = new Team();
+		schiri = new Schiedsrichter();
+		spiel = new Spiel(t1,t2,schiri);
+		for(int i = 0; i < 5400; i++){
+			spiel.tick();
+		//	try {
+		//		Thread.sleep(200);
+		//	} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+		//		e.printStackTrace();
+		//	}
+		}
 		
 	}
 	
