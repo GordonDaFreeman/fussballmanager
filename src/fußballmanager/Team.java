a87e8ba3cb9b901aebdbfeeb6d32e65b40743942
delta1805 <31958216+delta1805@users.noreply.github.com>
2017-09-21 12:55:44 +0200

    Update Team.java

--- src/fußballmanager/Team.java
+++ src/fußballmanager/Team.java
@@ -1,7 +1,7 @@
-package fußballmanager;
+package fuÃŸballmanager;
 
-import fußballmanager.namegen.Datenbank;
-import fußballmanager.personen.*;
+import fuÃŸballmanager.namegen.Datenbank;
+import fuÃŸballmanager.personen.*;
 
 public class Team {
 	String name;
@@ -12,27 +12,66 @@ public class Team {
 	
 	public Team(String name){
 		this.name = name;
-		fülleTeam();
+		fÃ¼lleTeam();
 		addTrainer();
 	}
 	
 	public Team(){
 		this.name = Datenbank.genName()+"'s Team";
-		fülleTeam();
+		fÃ¼lleTeam();
 		addTrainer();
 	}
 	
-	public void fülleTeam(){
-		//Fülle Feldspieler
+	public void fÃ¼lleTeam(){
+		//FÃ¼lle Feldspieler
 		for(int i =0; i<10;i++){
 			spieler[i] = new Spieler();
 		}
-		//Fülle Torwart
+		//FÃ¼lle Torwart
 		torwart = new Torwart();
 	}
 	
 	public void addTrainer(){
 		trainer = new Trainer();
+	}
+	
+	public void setPositions(boolean heim){
+		if (heim){
+			torwart.p.x = 5;
+			torwart.p.y = 22.5;
+			for(int i = 0; i < 3; i++){
+				spieler[i].p.x = 20;
+				spieler[i].p.y = 7.5+15*i;
+			}
+			for(int i = 3; i < 7; i++){
+				spieler[i].p.x = 38;
+				spieler[i].p.y =5.625+11.25*i;
+			}
+			for(int i = 7; i < 9; i++){
+				spieler[i].p.x = 52;
+				spieler[i].p.y =11.25+22.5*i;
+			}
+			spieler[9].p.x=44;
+			spieler[9].p.y=45;
+		}else{
+			torwart.p.x = 90-5;
+			torwart.p.y = 22.5;
+			for(int i = 0; i < 3; i++){
+				spieler[i].p.x = 90-20;
+				spieler[i].p.y = 7.5+15*i;
+			}
+			for(int i = 3; i < 7; i++){
+				spieler[i].p.x = 90-38;
+				spieler[i].p.y =5.625+11.25*i;
+			}
+			for(int i = 7; i < 9; i++){
+				spieler[i].p.x = 90-52;
+				spieler[i].p.y =11.25+22.5*i;
+			}
+			spieler[9].p.x=46;
+			spieler[9].p.y=45;
+		}
+			
 	}
 	
 	public boolean isInTeam(Person p){
@@ -43,3 +82,4 @@ public class Team {
 		return false;
 	}
 }
+
